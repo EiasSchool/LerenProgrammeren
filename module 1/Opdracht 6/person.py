d7b49cfa5ece23fa1071c13229b653d5605a8cbc
Eias <51035404+1Eias@users.noreply.github.com>
2024-09-10 12:11:01 +0200

    M1.O6

---
+++ module 1/Opdracht 6/person.py
@@ -0,0 +1,11 @@
+naam = input('Wat is je naam?\n')
+adres = input('Wat is jouw adres?\n')
+postcode = input('Wat is jouw postcode?\n')
+woonplaats = input('Wat is jouw woonplaats?\n')
+
+print(f''' ----------------------------------------------------
+|  Naam      : {naam}                
+|  Adres     : {adres}           
+|  Postcode  : {postcode}                         
+|  Woonplaats: {woonplaats}                          
+ ----------------------------------------------------''')
