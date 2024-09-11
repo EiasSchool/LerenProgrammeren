bd1781777bc02165a192e56f6b35e4751cd93740
Eias <51035404+1Eias@users.noreply.github.com>
2024-09-11 18:53:47 +0200

    M1.O8

---
+++ module 1/Opdracht 8/question.py
@@ -0,0 +1,5 @@
+naam = input('Hallo, wat is je naam? ')
+leeftijd = int(input(f'Goedendag {naam}, hoe oud ben je? '))
+fav_eten = input(f'En als {leeftijd} jarige, wat eet je het liefst? ')
+drinken = input(f'En wat drink je het liefst bij {fav_eten}? ')
+print(f'\nDe {leeftijd} jarige {naam} drinkt het liefst {drinken} bij {fav_eten}')
