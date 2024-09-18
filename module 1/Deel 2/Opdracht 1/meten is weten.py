769b962eb6efacef2ed48220f4d557b3f8da029d
Eias <51035404+1Eias@users.noreply.github.com>
2024-09-18 10:59:20 +0200

    Min en Max

--- module 1/Deel 2/Opdracht 1/meten is weten.py
+++ module 1/Deel 2/Opdracht 1/meten is weten.py
@@ -3,9 +3,15 @@ b = int(input('Wat is de tweede getal?'))
 
 if a > b:
     Max = a
+    Min = b
     print(f'a is het grootste getal: {Max}')
+    print(f'Het minimum is: {Min}')
+    print(f'Het maximum is: {Max}')
 elif a < b:
     Min = a
+    Max = b
     print(f'a is het kleinste getal: {Min}')
+    print(f'Het minimum is: {Min}')
+    print(f'Het maximum is: {Max}')
 else:
     print('a en b zijn even groot')
