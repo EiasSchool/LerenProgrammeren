060114855ffde55dd0a785cf56ffd4d4b865b5e8
Eias <51035404+1Eias@users.noreply.github.com>
2024-10-01 10:59:31 +0200

    M1.D3.O7

---
+++ module 1/Deel 3/Opdracht 7/exceptional_bugs.py
@@ -0,0 +1,21 @@
+import random
+
+#selecteer 2 nummers
+num1 = random.randint(1,10)
+num2 = random.randint(5,15)
+
+#vraag om een antwoord
+number = input(f'Weet jij wat {num1} + {num2} is? ') 
+
+#geef reactie op het antwoord
+while True:
+    try:
+        if int(number) == (num1 + num2):
+            print('Dat is juist')
+            break
+        else:
+            print('Nee dat klopt niet')
+            break
+    except ValueError:
+        print('Dat is geen nummer!')
+        break
