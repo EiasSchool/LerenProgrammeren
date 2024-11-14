0215991b763eda41bf213f367c5747e9e2678e5c
Eias <51035404+1Eias@users.noreply.github.com>
2024-11-14 11:16:30 +0100

    M2.D2.O2

---
+++ Module 2/Deel 2/Opdracht 2/M&M's.py
@@ -0,0 +1,19 @@
+import random
+from collections import Counter
+
+kleuren = ('oranje', 'blauw', 'groen', 'bruin')
+zak = []
+
+hoeveelheid = int(input('Hoeveel M&M\'s wil je aan de zak toevoegen? '))
+
+for M in range(hoeveelheid):
+    random_color = random.choice(kleuren)
+    zak.append(random_color)
+
+print(f'De zak bevat de volgende M&M\'s:')
+
+color_counts = Counter(zak)
+
+for color, count in color_counts.items():
+    print(f"{color}: {count}")
+
