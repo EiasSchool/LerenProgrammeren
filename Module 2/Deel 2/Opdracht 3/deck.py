3186c43f3308490b1d8050b67b381e4b14c276a5
Eias <51035404+1Eias@users.noreply.github.com>
2024-11-14 12:10:43 +0100

    M2.D2.O3

---
+++ Module 2/Deel 2/Opdracht 3/deck.py
@@ -0,0 +1,17 @@
+import random
+
+kleuren = ['harten', 'klaveren', 'schoppen', 'ruiten']
+rangen = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'boer', 'vrouw', 'heer', 'aas']
+
+deck = [f'{rang} van {kleur}' for rang in rangen for kleur in kleuren] + ['jocker', 'jocker']
+
+random.shuffle(deck)
+
+eerste_7_kaarten = deck[:7]
+overgebleven_kaarten = deck[7:]
+
+for kaart in range(7):
+    print(f'kaart {kaart + 1}: {eerste_7_kaarten[kaart]}')
+
+print(f'\nAantal overgebelven kaarten: {len(overgebleven_kaarten)}')
+print(f'Overgebleven kaarten: {overgebleven_kaarten}')
