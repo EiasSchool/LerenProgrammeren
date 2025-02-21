67d2ff3671e5d819197fd55e74ca9a427449d1df
Eias <51035404+1Eias@users.noreply.github.com>
2025-02-21 11:03:33 +0100

    M3.P3.O4 fix

--- Module 3/Proef Module 3/Opdracht 4/Realisatie.py
+++ Module 3/Proef Module 3/Opdracht 4/Realisatie.py
@@ -3,48 +3,50 @@ import random
 namen = []
 
 while len(namen) < 3:
-    naam = input("Geef een naam:\n")
+    naam = input("Geef een naam\n").strip().lower()
     if naam not in namen:
         namen.append(naam)
     else:
-        print('Deze naam bestaat al, probeer een andere naam.')
+        print('Deze naam bestaat al probeer een andere naam')
 
 while True:
-    verder = input("Wil je nog een naam toevoegen? (ja/nee)\n").strip().lower()
+    verder = input("Wil je nog een naam toevoegen ja/nee\n").strip().lower()
     if verder == "nee":
         break
     elif verder == 'ja':
-        naam = input("Geef een naam:\n")
+        naam = input("Geef een naam\n").strip().lower()
         if naam not in namen:
             namen.append(naam)
         else:
-            print("Deze naam bestaat al, probeer een andere naam.")
+            print("Deze naam bestaat al probeer een andere naam")
     else:
-        print("Antwoord met 'ja' of 'nee'.")
+        print("Antwoord met ja of nee")
 
 lootjes = namen.copy()
 random.shuffle(lootjes)
 
 while True:
     random.shuffle(lootjes)
-    zelfgetrokken = False
+    goed = True
     for i in range(len(namen)):
         if lootjes[i] == namen[i]:
-            zelfgetrokken = True
+            goed = False
             break
-    if not zelfgetrokken:
+    if goed:
         break
 
-results = dict(zip(namen, lootjes))
+results = {}
+for i in range(len(namen)):
+    results[namen[i]] = lootjes[i]
 
-print("\nDe lootjes zijn getrokken! Wie wie heeft, blijft geheim. Je kunt nu een naam opgeven om te vragen wie ze hebben.")
+print("De lootjes zijn getrokken je kunt nu een naam opgeven om te vragen wie ze hebben\n")
 
 while True:
-    findName = input("\nWelke naam wil je opzoeken? (type 'stop' om te stoppen): ").strip()
-    if findName.lower() == 'stop':
-        print("Het programma wordt afgesloten.")
+    findName = input("Welke naam wil je opzoeken type stop om te stoppen\n").strip().lower()
+    if findName == 'stop':
+        print("Het programma wordt afgesloten")
         break
     elif findName in results:
-        print(f"{findName} heeft {results[findName]} getrokken.")
+        print(f"{findName.capitalize()} heeft {results[findName].capitalize()} getrokken")
     else:
-        print("Deze naam is niet gevonden. Probeer het opnieuw.")
+        print("Deze naam is niet gevonden Probeer het opnieuw")
