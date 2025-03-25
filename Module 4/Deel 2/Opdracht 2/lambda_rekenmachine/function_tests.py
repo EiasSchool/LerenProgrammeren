57a3a6b9b0cebc5e019edb838dc0dcf27179b597
Eias <51035404+1Eias@users.noreply.github.com>
2025-03-25 10:46:24 +0100

    M4.D2.O2 S2

--- Module 4/Deel 2/Opdracht 2/lambda_rekenmachine/function_tests.py
+++ Module 4/Deel 2/Opdracht 2/lambda_rekenmachine/function_tests.py
@@ -1,3 +1,24 @@
 from functions import addition, subtraction, multiplication, division
 from calculations import CALCULATIONS
 from test_lib import test, report
+
+getal1 = 3.0
+getal2 = 2.0
+
+expected = addition(getal1, getal2)
+result_lambda = CALCULATIONS['addition'](getal1, getal2)
+test('Test addition function', expected, result_lambda)
+
+expected = subtraction(getal1, getal2)
+result_lambda = CALCULATIONS['subtraction'](getal1, getal2)
+test('Test subtraction function', expected, result_lambda)
+
+expected = multiplication(getal1, getal2)
+result_lambda = CALCULATIONS['multiplication'](getal1, getal2)
+test('Test multiplication function', expected, result_lambda)
+
+expected = division(getal1, getal2)
+result_lambda = CALCULATIONS['division'](getal1, getal2)
+test('Test division function', expected, result_lambda)
+
+report()
