b8f3bc2a37fbf838788487f954d5028142dca6a2
Eias <51035404+1Eias@users.noreply.github.com>
2025-04-04 10:59:57 +0200

    M4.P4.O8

---
+++ Module 4/Proef Module 4/Opdracht 8/Tests/test_O04.py
@@ -0,0 +1,18 @@
+import sys, os
+from test_lib import test, report
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+
+from data import JOURNEY_IN_DAYS
+
+#schrijf je test hier
+
+expected = True
+
+if JOURNEY_IN_DAYS == 11:
+    resault = True
+
+test('Journey in days - test', expected, resault)
+
+if __name__ == "__main__":
+    report()
